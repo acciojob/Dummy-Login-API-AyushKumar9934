5833d858728295bc178c48120141a466cfa71492
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-12 15:47:11 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,85 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+const data = [
+  {
+    id: 1,
+    name: "ABC",
+    email: "abc@gmail.com",
+    password: "12",
+  },
+  {
+    id: 2,
+    name: "DEF",
+    email: "def@gmail.com",
+    password: "1234",
+  },
+  {
+    id: 3,
+    name: "GHI",
+    email: "ghi@gmail.com",
+    password: "123456",
+  },
+];
 
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userError,setUserError]=useState("");
+  const [passError,setPassError]=useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("we are inside handle submit")
+    let filterdata =
+    data.filter((item) => {
+      return item.email == email;
+    })
+    setTimeout(() => {
+      
+        
+        filterdata.length>0 ? (
+          filterdata[0].password == password ? (
+            console.log(filterdata[0])
+          ) : (
+            setPassError("Password Incorrect")
+           
+          )
+        ) : (
+
+            setUserError("User not found")
+         
+        );
+    }, 3000);
+  }
+
+
+  return (
+    <form>
+      <input
+        value={email}
+        placeholder="email"
+        type="text"
+        id="input-email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br></br>
+     { userError&& <div id="user-error">{userError}</div> }
+      <input
+        value={password}
+        type="text"
+        placeholder="password"
+        id="input-password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br></br>
+      {passError&& <div id="password-error">{passError}</div>}
+      <button
+        onClick={(e) => handleSubmit(e)}
+        type="submit"
+        id="submit-form-btn"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default App;
